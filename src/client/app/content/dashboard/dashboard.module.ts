d93194b5843ab257654a672162236697e54e7b05
oelabed <oel3abed@gmail.com>
2017-04-29 21:01:01 +0200

    change feature category to component

--- src/client/app/content/dashboard/dashboard.module.ts
+++ src/client/app/content/dashboard/dashboard.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 
-import { ProductLiveModule } from '../../shared/modules/product/live/productLive.module';
+import { BidDashboardLiveModule } from '../../shared/modules/bid/dashboard-live/bidDashboardLive.module';
 import { BreadcrumbModule } from '../../shared/modules/layout/breadcrumb/breadcrumb.module';
 
 import { BidService } from '../../shared/services/bid/bid.service';
 import { BreadcrumbService } from '../../shared/services/breadcrumb/breadcrumb.service';
 
 @NgModule({
-    imports: [CommonModule, ProductLiveModule, BreadcrumbModule],
+    imports: [CommonModule, BidDashboardLiveModule, BreadcrumbModule],
     declarations: [DashboardComponent],
     exports: [DashboardComponent],
     providers: [BidService, BreadcrumbService]
--- src/client/app/shared/modules/category/feature/categoryFeature.module.ts
+++ src/client/app/shared/modules/category/feature/categoryFeature.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { BidFeatureModule } from '../../bid/feature/bidFeature.module';
+
 import { CategoryFeatureComponent } from './index';
 
 
 @NgModule({
-    imports: [CommonModule, RouterModule],
+    imports: [CommonModule, RouterModule, BidFeatureModule],
     declarations: [CategoryFeatureComponent],
     exports: [CategoryFeatureComponent]
 })
