808850febd1b597c12f9fe2d76260618affcb921
oelabed <oel3abed@gmail.com>
2017-05-18 15:41:57 +0200

    add paging categories live and feature

--- src/client/app/shared/modules/category/feature/categoryFeature.component.ts
+++ src/client/app/shared/modules/category/feature/categoryFeature.component.ts
@@ -2,8 +2,12 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 import { BidService } from '../../../../shared/services/bid/bid.service';
 
+import { OnPaging, PageData } from '../../layout/list/paging';
+
 import { Category } from '../../../../shared/models/category';
 import { Bid } from '../../../../shared/models/bid';
+
+const PAGES_COLLECTIONS_SIZE: number = 4;
 
 @Component({
     moduleId: module.id,
@@ -11,11 +15,14 @@ import { Bid } from '../../../../shared/models/bid';
     templateUrl: 'category-feature.component.html',
 })
 
-export class CategoryFeatureComponent implements OnChanges {
+export class CategoryFeatureComponent implements OnChanges, OnPaging {
 
     bids: Bid[];
     errorMessage: string = '';
     isLoading: boolean = true;
+
+    pageData: PageData = { current: 1, pages_size: 10, page_collections_size: PAGES_COLLECTIONS_SIZE };
+
 
     @Input()
     data: Category;
@@ -34,13 +41,24 @@ export class CategoryFeatureComponent implements OnChanges {
 
         if (category) {
 
-            this.bidService
-                .getFeatureByCategoryPage(this.data.id, 1, 3)
-                .subscribe(
+            this.loadBids();
+        }
+    }
+
+    onPageChange(event: any) {
+        this.pageData.current = event;
+        console.debug(this.pageData.current);
+        this.loadBids();
+    }
+
+    loadBids() {
+
+        this.bidService
+            .getFeatureByCategoryPage(this.data.id, this.pageData.current, 3)
+            .subscribe(
          /* happy path */ p => this.bids = p,
          /* error path */ e => this.errorMessage = e,
          /* onComplete */() => this.isLoading = false);
-        }
     }
 }
 
--- src/client/app/shared/modules/category/live/categoryLive.component.ts
+++ src/client/app/shared/modules/category/live/categoryLive.component.ts
@@ -2,8 +2,12 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 import { BidService } from '../../../../shared/services/bid/bid.service';
 
+import { OnPaging, PageData } from '../../layout/list/paging';
+
 import { Category } from '../../../../shared/models/category';
 import { Bid } from '../../../../shared/models/bid';
+
+const PAGES_COLLECTIONS_SIZE: number = 4;
 
 @Component({
     moduleId: module.id,
@@ -11,11 +15,14 @@ import { Bid } from '../../../../shared/models/bid';
     templateUrl: 'category-live.component.html',
 })
 
-export class CategoryLiveComponent implements OnChanges {
+export class CategoryLiveComponent implements OnChanges, OnPaging {
 
     bids: Bid[];
     errorMessage: string = '';
     isLoading: boolean = true;
+
+
+    pageData: PageData = { current: 1, pages_size: 10, page_collections_size: PAGES_COLLECTIONS_SIZE };
 
     @Input()
     data: Category;
@@ -33,13 +40,24 @@ export class CategoryLiveComponent implements OnChanges {
     getLiveBid(category: Category) {
 
         if (category) {
+            this.loadBids();
 
-            this.bidService
-                .getByCategoryPage(this.data.id, 1, 4)
-                .subscribe(
+        }
+    }
+
+    onPageChange(event: any) {
+        this.pageData.current = event;
+        console.debug(this.pageData.current);
+        this.loadBids();
+    }
+
+    loadBids() {
+        this.bidService
+            .getByCategoryPage(this.data.id, this.pageData.current, 4)
+            .subscribe(
          /* happy path */ p => this.bids = p,
          /* error path */ e => this.errorMessage = e,
          /* onComplete */() => this.isLoading = false);
-        }
+
     }
 }
